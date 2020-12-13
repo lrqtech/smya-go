44ac37cf9dc7beb3d0daa0387f15739ae7f20cac
lrqtech <lrqtech@126.com>
2020-12-13 20:25:43 +0800

    update

--- core/request.go
+++ core/request.go
@@ -25,7 +25,6 @@ type Response struct {
 func Login() (string, string) {
 	str := ResEncrypt()
 	url := "https://smya.cn/client/login"
-	header := "{\"Content-Type\": \"application/json\"}"
 	data := Info{
 		DeviceId: config.DeviceId,
 		Str:      str,
@@ -34,7 +33,7 @@ func Login() (string, string) {
 	if err != nil {
 		fmt.Println("生成json字符串错误")
 	}
-	req := util.Request("POST", url, body, header)
+	req := util.Request("POST", url, body, 108)
 	var r Response
 	_ = json.Unmarshal(req, &r)
 	return r.Data["server"], r.Data["subscribe"]
