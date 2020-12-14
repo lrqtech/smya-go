d98d65b767e70551610f94c37f716f202bd6e660
lrqtech <lrqtech@126.com>
2020-12-14 11:54:02 +0800

    update

--- core/exec.go
+++ core/exec.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 	"runtime"
+	"syscall"
 )
 
 // 执行命令
@@ -11,19 +12,31 @@ func ExecShell(tp int, shell string) {
 	if tp == 5 {
 		switch runtime.GOOS {
 		case "linux":
-
-		case "windows":
-			cmd := exec.Command("cmd.exe", "/c", "start "+shell)
-			// cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+			cmd := exec.Command("/bin/bash", "-c", shell)
 			err := cmd.Run()
 			if err != nil {
 				fmt.Println(err)
 			}
+			fmt.Println("Command has been executed")
+		case "windows":
+			cmd := exec.Command("cmd.exe", "/c", "start "+shell)
+			cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Command has been executed")
 		case "darwin":
-
+			cmd := exec.Command("/usr/bin/", "-c", shell)
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Command has been executed")
 		}
+	} else {
+		fmt.Println("Command dose not support")
+		fmt.Println("Please use GUI program")
+		fmt.Println("More information: https://smya.cn/download")
 	}
-	fmt.Println("Command dose not support")
-	fmt.Println("Please use GUI program")
-	fmt.Println("More information: https://smya.cn/download")
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"smya/core"
 )
 
 func main() {
+	fmt.Println("Starting ......")
 	server, subscribe := core.Login()
 	client := core.StartMqtt(server, subscribe)
 	for {
