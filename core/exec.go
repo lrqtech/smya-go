6cca8fc19a67b2c5a20782e7669bd415a67db3e6
lrqtech <lrqtech@126.com>
2020-12-18 13:36:32 +0800

    加入远程开机功能

--- core/exec.go
+++ core/exec.go
@@ -7,30 +7,34 @@ import (
 )
 
 // 执行命令
-func ExecShell(tp int, shell string) {
+func ExecShell(tp int, shell string, name string) {
 	if tp == 5 {
-		switch runtime.GOOS {
-		case "linux":
-			cmd := exec.Command("/bin/bash", "-c", shell)
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println(err)
+		if name == "wol" || name == "WOL" {
+			WolWake(shell, "", "", "")
+		} else {
+			switch runtime.GOOS {
+			case "linux":
+				cmd := exec.Command("/bin/bash", "-c", shell)
+				err := cmd.Run()
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println("Command has been executed")
+			case "windows":
+				cmd := exec.Command("cmd.exe", "/c", "start "+shell)
+				err := cmd.Run()
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println("Command has been executed")
+			case "darwin":
+				cmd := exec.Command("/usr/bin/", "-c", shell)
+				err := cmd.Run()
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println("Command has been executed")
 			}
-			fmt.Println("Command has been executed")
-		case "windows":
-			cmd := exec.Command("cmd.exe", "/c", "start "+shell)
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Command has been executed")
-		case "darwin":
-			cmd := exec.Command("/usr/bin/", "-c", shell)
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Command has been executed")
 		}
 	} else {
 		fmt.Println("Command dose not support")
