01ce60028726d18ebcf48440d3c73c73d3090ac7
lrqtech <lrqtech@126.com>
2020-12-18 16:17:11 +0800

    优化程序

--- config/init.go
+++ config/init.go
@@ -25,12 +25,14 @@ func init() {
 		if len(*CliDeviceId) != 32 {
 			fmt.Printf("The length of device id %s isn't enough \n ", *CliDeviceId)
 			fmt.Println("Please try again")
+			os.Exit(0)
 		} else {
 			DeviceId = *CliDeviceId
 		}
 		if len(*CliKey) != 6 {
 			fmt.Printf("The length of safe code %s isn't enough \n ", *CliKey)
 			fmt.Println("Please try again")
+			os.Exit(0)
 		} else {
 			Key = *CliKey
 		}
