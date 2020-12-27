8f68cbe0d2d62fbc32c7a56aa0d09201ad6e176f
lrqtech <lrqtech@126.com>
2020-12-27 16:35:08 +0800

    更新

--- core/publish.go
+++ core/publish.go
@@ -6,18 +6,12 @@ import (
 	"encoding/json"
 	"smya/config"
 	"smya/model"
-	"strconv"
 	"time"
 )
 
 // 生成发布地址
 func GenPubAddr() string {
-	loc, _ := time.LoadLocation("Asia/Shanghai")
-	CurrentHour := strconv.Itoa(time.Now().In(loc).Hour())
-	if len(CurrentHour) == 1 {
-		CurrentHour += "0"
-	}
-	sum256 := sha256.Sum256([]byte(config.DeviceId + CurrentHour + config.Key))
+	sum256 := sha256.Sum256([]byte(config.DeviceId + config.Key))
 	return base64.RawStdEncoding.EncodeToString(sum256[:])
 }
 
